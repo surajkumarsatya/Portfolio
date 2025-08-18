489cb72655f949aae3405a19d8414e6265dcaadb
Suraj <surajmail.799@gmail.com>
2025-08-19 00:54:39 +0530

    download pdf link change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ const Header = () => {
   return (
     <div className="flex justify-end items-center">
       <div className="flex gap-2">
-        <a href="https://surajkumarsatya.netlify.app/assets/Suraj%20Kumar%20Satya_Frontend%20Developer_Fresher.pdf"
+        <a href="https://drive.google.com/file/d/1Ke-fHj6B2QaQ6gg14cmfAODWuAwQAVC0/view?usp=drive_link"
          target="_blank" className="bg-teal-600 text-white border border-white px-4 py-2 my-5 text-sm
          hover:bg-white hover:text-teal-600 hover:border hover:border-teal-600 transition-colors">Download CV ⤓</a>
         {/* <button className="bg-teal-600 text-white border border-white rounded-lg px-4 py-2 text-sm hover:bg-gray-100 transition-colors">LANG 🌐</button> */}
